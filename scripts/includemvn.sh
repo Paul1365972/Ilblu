b25cbea7cac0538bf8efb36bb77db20d4277417a
Paul1365972 <paul1365972@gmail.com>
2020-04-22 14:48:14 +0200

    Auto maven installtion

---
+++ scripts/includemvn.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+cd "$basedir/"
+echo "Including maven..."
+
+if ! mvn -v 2> /dev/null; then
+    echo "mvn missing"
+    if [ ! -d "apache-maven-3.6.3" ]; then
+        echo "Downloading maven"
+        curl -s http://apache.mirror.iphh.net/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz | tar -xzf -
+    fi
+    export PATH=$PATH:$basedir/apache-maven-3.6.3/bin
+    echo "Exported mvn"
+fi
